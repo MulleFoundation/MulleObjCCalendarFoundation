7a0c4cc9dd0f675fbfad816dcf94f281f43a3088
Nat! <nat@mulle-kybernetik.com>
2020-03-31 23:58:43 +0200

    updated travis

--- cmake/_Dependencies.cmake
+++ cmake/_Dependencies.cmake
@@ -69,7 +69,7 @@ if( NOT MULLE_OBJC_OS_FOUNDATION_LIBRARY)
          endif()
       endforeach()
       #
-      # Search for "objc-loader.inc" in include directory.
+      # Search for "MulleObjCLoader+<name>.h" in include directory.
       # Disable with: `mulle-sourcetree mark MulleObjCOSFoundation no-cmakeloader`
       #
       if( NOT NO_INHERIT_OBJC_LOADERS)
